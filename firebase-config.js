8aae46b727504aa9ae9a4660b34e3bd6d88881a5
Jorge Echeverria <jfloresjef365@gmail.com>
2025-10-31 22:23:38 -0600

    Update Firebase configuration and initialization

--- firebase-config.js
+++ firebase-config.js
@@ -6,13 +6,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/12.4.0/firebase
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCctj5hkLTsgJyYVDuDpP_Wejm9nfnLmJ8",
-  authDomain: "fir-2b58c.firebaseapp.com",
-  projectId: "fir-2b58c",
-  storageBucket: "fir-2b58c.firebasestorage.app",
-  messagingSenderId: "585010347515",
-  appId: "1:585010347515:web:c7ffddce2809a8320a5f5e",
-  measurementId: "G-6R55F20XQP"
+  apiKey: "AIzaSyCKePTDIxp_H8hFpGm-8L3F6_fE_yZE-Lc",
+  authDomain: "fir-ayudantia-6ecac.firebaseapp.com",
+  projectId: "fir-ayudantia-6ecac",
+  storageBucket: "fir-ayudantia-6ecac.firebasestorage.app",
+  messagingSenderId: "322492795465",
+  appId: "1:322492795465:web:1e10b6838c466899ee8d98",
+  measurementId: "G-K61RC6ZX2N"
 };
 
 // Initialize Firebase
